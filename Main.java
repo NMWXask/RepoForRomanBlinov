6df582fe3f48dee937302e47b7a26ff77dfcfa74
NMWXask <k.marcus@mail.ru>
2023-08-17 19:16:55 +0400

    Updated methods

--- Main.java
+++ Main.java
@@ -18,5 +18,13 @@ public class Main {
             }
         }
         System.out.println(maxEl);
+        sredArifm(mass);
+    }
+    public static void sredArifm(int[]arr){
+        int sum=0;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+        }
+        System.out.println(sum/ arr.length);
     }
 }
