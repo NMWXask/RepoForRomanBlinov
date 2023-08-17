cf79e8ff1d1b4ede9f680c28754616cbdfcb7df7
NMWXask <k.marcus@mail.ru>
2023-08-17 20:35:55 +0400

    New metthod checkEvenNumber

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Привет Роман!");
         System.out.println("Работа с массивом для Git тренировки");
         System.out.println("====================================");
         Random random=new Random();
